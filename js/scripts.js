f3d52e5cd9b7108fdf1774291b9dce5fa3cb7906
Amin Moeini <amin47moeini@gmail.com>
2025-11-29 19:32:50 +0330

    script: add header and navbar scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
 "use strict";
+
+// Elements
+const menuBtn = document.querySelector(".header-toggle");
+const menuList = document.querySelector(".header-list");
+const overlay = document.querySelector(".overlay");
+
+// Add logic to menu button
+menuBtn.addEventListener("click", function () {
+  menuBtn.classList.toggle("header-toggle--show");
+  menuList.classList.toggle("header-list--show");
+  overlay.classList.toggle("overlay--show");
+});
+
+overlay.addEventListener("click", function () {
+  menuBtn.classList.remove("header-toggle--show");
+  menuList.classList.remove("header-list--show");
+  overlay.classList.remove("overlay--show");
+});
